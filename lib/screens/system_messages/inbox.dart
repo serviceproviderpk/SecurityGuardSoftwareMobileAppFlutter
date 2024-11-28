56881dfecb2e6859b5861e6636725032a4ed6934
ukkhhan <ukkhan647@gmail.com>
2024-11-28 15:39:54 +0500

    Security Management System

--- lib/screens/system_messages/inbox.dart
+++ lib/screens/system_messages/inbox.dart
@@ -2,30 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:securitymanagementsystem/Screens/deshboard.dart';
 import 'package:securitymanagementsystem/providers/app_bar_notifier.dart';
+
 import '../../widgets/app_bar.dart';
-import 'system_msgs.dart';
 
 class InboxScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     // calling notifier
     final appBarNotifier = Provider.of<AppBarNotifier>(context, listen: false);
 
     //set values
     appBarNotifier.setTitle('Inbox');
     appBarNotifier.setActions([
-      IconButton(onPressed: (){}, icon: const Icon(Icons.search_rounded)),
-      IconButton(onPressed: (){
-        Navigator.of(context).push(MaterialPageRoute(builder: (_) => const Dashboard()));
-      }, icon: const Icon(Icons.home)),
+      IconButton(onPressed: () {}, icon: const Icon(Icons.search_rounded)),
+      IconButton(
+          onPressed: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (_) => const Dashboard()));
+          },
+          icon: const Icon(Icons.home)),
     ]);
 
     return Scaffold(
-      appBar: PreferredSize(
+      appBar: const PreferredSize(
         preferredSize: Size.fromHeight(kToolbarHeight),
-        child: CustomAppBar(
-        ),
+        child: CustomAppBar(),
       ),
       body: ListView.builder(
         itemCount: 20,
